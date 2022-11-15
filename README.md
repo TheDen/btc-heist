6ec4c6d5cde8975299fa850486b8effe4a979320
TheDen <deniskhoshaba@gmail.com>
2022-11-16 02:48:11 +1100

    update btc addresses source

--- README.md
+++ README.md
@@ -5,26 +5,24 @@
 
 Install deps, i.e., `python3 -m pip install -r requirements.txt`
 
-Download the [CSV dump of all bitcoin addresses with a balance](https://bitkeys.work/download.php) and `cut` the first column to make a file of BTC address
+Download the [lastest list of all funded BTC addresses](http://addresses.loyce.club/)
 
 ```bash
-wget 'https://bitkeys.work/btc_balance_sorted.csv'
-cut -d, -f 1 btc_balance_sorted.csv | grep -v address > public_addresses_sorted.txt
+wget 'http://addresses.loyce.club/Bitcoin_addresses_LATEST.txt.gz'
 ```
 
 then run `python3 btc-heist.py`
 
-
 ```
-usage: btc-heist.py [-h] [-c CORES] [-f ADDRESSES] [-o KEYFILE] [-d]
+usage: btc-heist.py [-h] [-c CORES] [-f ADDRESSES] [-o KEYFILE]
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -c CORES, --cores CORES
                         Number of CPU cores to use (default: 4)
   -f ADDRESSES, --addresses ADDRESSES
-                        File containing BTC addresses (default: public_addresses_sorted.txt),
+                        File containing BTC addresses (default:
+                        public_addresses_sorted.txt),
   -o KEYFILE, --keyfile KEYFILE
                         File to output found keys (default: found_keys.txt)
-  -d, --download        Download and parse CSV file of bitcoin addresses
 ```
--- btc-heist.py
+++ btc-heist.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import progressbar
 import csv
@@ -8,50 +10,14 @@ import multiprocessing
 from bitcoin import sha256, privtopub, pubtoaddr
 from mnemonic import Mnemonic
 
-btc_addresses_url = "https://bitkeys.work/btc_balance_sorted.csv"
-download_filename = "btc_balance_sorted_temp.csv"
-pbar = None
-
-
-def show_progress(block_num, block_size, total_size):
-    global pbar
-    if pbar is None:
-        pbar = progressbar.ProgressBar(maxval=total_size)
-        pbar.start()
-    downloaded = block_num * block_size
-    if downloaded < total_size:
-        pbar.update(downloaded)
-    else:
-        pbar.finish()
-        pbar = None
-
-
-def download_address_csv(btc_addresses_url, download_filename):
-    urllib.request.urlretrieve(
-        btc_addresses_url,
-        download_filename,
-        show_progress,
-    )
-
-
-def parse_and_save_csv(download_filename, address_list_filename):
-    rows = []
-    with open(download_filename, newline="") as csv_file:
-        for row in csv.reader(csv_file, delimiter=","):
-            if row[0] != "address":
-                rows.append(row[0])
-    with open(address_list_filename, "w+") as parsed_addresses:
-        list(map(lambda item: parsed_addresses.write(f"{item}\n"), rows))
-
 
 def seek(core, btc_address_queue):
     print(f"Core {core}: Searching for Private Key...")
     mnemo = Mnemonic("english")
-    log_rate_iterations = 10000
+    log_rate_iterations = 1000
     start_time = datetime.today().timestamp()
 
     for iteration in count(1):
-
         # Generate private + public keys and btc address
         private_key = sha256(mnemo.generate(strength=256))
         public_key = privtopub(private_key)
@@ -61,7 +27,7 @@ def seek(core, btc_address_queue):
         # log rate
         if (iteration % log_rate_iterations) == 0:
             time_diff = datetime.today().timestamp() - start_time
-            print(f"Core {core}: {iteration / time_diff} Key/s")  # 253 Key/s
+            print(f"Core {core}: {iteration / time_diff} Key/s")
 
 
 if __name__ == "__main__":
@@ -77,7 +43,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-f",
         "--addresses",
-        default="public_addresses_sorted.txt",
+        default="Bitcoin_addresses_LATEST.txt",
         type=str,
         help="File containing BTC addresses (default: public_addresses_sorted.txt),",
     )
@@ -88,27 +54,11 @@ if __name__ == "__main__":
         type=str,
         help="File to output found keys (default: found_keys.txt)",
     )
-    parser.add_argument(
-        "-d",
-        "--download",
-        default=False,
-        action="store_true",
-        help="Download and parse CSV file of bitcoin addresses",
-    )
 
     args = parser.parse_args()
     cores = args.cores
     addresses_filename = args.addresses
     keyfile = args.keyfile
-    download = args.download
-
-    if download:
-        print(f"Donloading {btc_addresses_url} as {download_filename}")
-        print(btc_addresses_url)
-        download_address_csv(btc_addresses_url, download_filename)
-        print("Finished downloading")
-        print(f"Parsing {download_filename} and saving it as {addresses_filename} ...")
-        parse_and_save_csv(download_filename, addresses_filename)
 
     # generate list of pubkey with BTC
     print(f'Loading "{addresses_filename}"...')
@@ -117,14 +67,12 @@ if __name__ == "__main__":
     print("Loaded.")
 
     btc_address_queue = multiprocessing.Queue()
-
     for core in range(cores):
         process = multiprocessing.Process(target=seek, args=(core, btc_address_queue))
         process.start()
 
     while True:
         private_key, public_key, btc_address = btc_address_queue.get()
-
         if f"{btc_address}\n" in publist:
             found_key = f"\nPublic: {public_key} | Private: {private_key} | Address: {btc_address}\n"
             print(found_key)
